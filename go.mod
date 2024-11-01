afffc717643b179cdec863ed1f60ffa1d3a8e96a
Mas.gan <reikan.kresna@gmail.com>
2024-11-01 23:29:45 +0700

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rknkrsnmln/say-hello-app.git
+
+go 1.23.2
+
+require github.com/rknkrsnmln/say-hello-go v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	sayhello "github.com/rknkrsnmln/say-hello-go"
+)
+
+func main() {
+	fmt.Println(sayhello.SayHello())
+}
